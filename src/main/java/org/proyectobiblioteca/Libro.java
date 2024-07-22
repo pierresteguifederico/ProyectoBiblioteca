5b249aade89c971fe4f2b30e2ed3a72f36dceb8d
pierresteguifederico <144521724+pierresteguifederico@users.noreply.github.com>
2024-07-22 17:34:49 -0300

    prototipo de gestión de biblioteca

---
+++ src/main/java/org/proyectobiblioteca/Libro.java
@@ -0,0 +1,33 @@
+package org.proyectobiblioteca;
+
+//Se define la plantilla libro con sus atributos y luego métodos
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int fechaPublicacion;
+
+//Se crea el constructor para inicializar el objeto Libro y se pasan por parámetro sus atributos
+    public Libro(String titulo, String autor, int fechaPublicacion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+//Se definen los getter para poder acceder controladamente a la información del objeto Libro
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+//Se define el método toString para proporcionar una representación del objeto Libro en forma de cadena de texto
+    @Override
+    public String toString() {
+        return "Título: " + titulo + ", Autor: " + autor + ", Año: " + fechaPublicacion;
+    }
+}
---
+++ src/test/java/BaseDeDatos.java
@@ -0,0 +1,38 @@
+
+package org.proyectobiblioteca;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BaseDeDatos {
+    private static final String URL = "jdbc:sqlite:biblioteca.db";
+
+    public static Connection Conectar() {
+        Connection conexion = null;
+        try {
+            conexion = DriverManager.getConnection(URL);
+            System.out.println("Conexion a la base de datos exitosa");
+            crearTabla(conexion);
+        } catch (SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+        return conexion;
+    }
+    public static void crearTabla (Connection conexion) {
+        String sql = "CREATE TABLE IF NOT EXISTS libros (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "titulo TEXT NOT NULL," +
+                "autor TEXT NOT NULL," +
+                "fechaPublicacion INTEGER NOT NULL," +
+                "UNIQUE (titulo, autor)" +
+                ");";
+        try (Statement statement = conexion.createStatement()) {
+                statement.execute(sql);
+        } catch (SQLException e) {
+                System.out.println("Error al crear la tabla: " + e.getMessage());
+            }
+
+        }
+    }
